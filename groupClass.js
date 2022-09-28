df5ee5956ecff1d1ca61ee989c93bdbd511ee3c1
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2022-09-28 22:12:56 +0200

    Create groupClass.js

---
+++ groupClass.js
@@ -0,0 +1,28 @@
+class Group {
+  constructor() {
+    this.elements = [];
+  }
+  
+  add(elem) {
+    if (this.elements.indexOf(elem) === -1) this.elements.push(elem);
+  }
+  
+  delete(elem) {
+    
+    const pos = this.elements.indexOf(elem);
+    
+    if(pos > -1) this.elements.splice(pos,1);
+  }
+  
+  has(elem) {
+    return (this.elements.indexOf(elem) !== -1 ? true : false);
+  }
+  
+  static from(obj) {
+    group = new Group();
+    for (let elem of obj) {
+      group.add(elem);
+    }
+    return group;
+  }
+}
