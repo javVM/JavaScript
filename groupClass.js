b5b25f55cfc8c89c1da3a778610bf7ae3cb9670c
Javier Verde Marín <73306559+javVM@users.noreply.github.com>
2022-09-28 22:30:59 +0200

    Update groupClass.js

--- groupClass.js
+++ groupClass.js
@@ -9,6 +9,7 @@ class Group {
   
   delete(elem) {
     const pos = this.elements.indexOf(elem);
+    
     if(pos > -1) this.elements.splice(pos,1);
   }
   
@@ -17,6 +18,7 @@ class Group {
   }
   
   static from(obj) {
+    
     let group = new Group();
     for (let elem of obj) {
       group.add(elem);
@@ -24,3 +26,43 @@ class Group {
     return group;
   }
 }
+
+class groupIterator {
+  constructor(group) {
+    this.group = group;
+    this.index = 0;
+  }
+  
+  next() {
+    if(this.index === this.group.elements.length) return { done : true };
+    
+    let value = this.group.elements[this.index];
+    this.index++;
+    
+    return { value, done : false };
+  }
+}
+
+Group.prototype[Symbol.iterator] = function() {
+  return new groupIterator(this);
+}
+
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(30);
+console.log(group.has(30));
+// → true
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
+
+for (let value of group) {
+  console.log(value);
+}
+// → 20
+// → 30
